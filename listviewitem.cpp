f214e88bb10e687f6b1a48653ff5d531eba374bc
Serpent Dragon <shelyganovdmitry@gmail.com>
2024-06-14 13:47:22 +0300

    Moved definition of ListViewItem to listviewitem.cpp

--- listviewitem.cpp
+++ listviewitem.cpp
@@ -1,3 +1,55 @@
 #include "listviewitem.h"
 
-// ListViewItem::ListViewItem() {}
+ListViewItem::ListViewItem(const QString &text, QWidget *parent) : QWidget(parent),
+    parent_(parent), width_(parent->size().width()), height_(LVI::Height)
+{
+    checkBox_ = new QCheckBox(this);
+    textEdit_ = new QTextEdit(text, this);
+    button_ = new QPushButton(this);
+
+    setupCheckBox();
+    setupTextEdit();
+    setupButton();
+
+    this->setLayout(setupLayout());
+    this->setMinimumSize(width_, height_);
+}
+
+void ListViewItem::setupCheckBox()
+{
+    checkBox_->setCheckState(Qt::Checked);
+    checkBox_->setFixedSize(LVI::CheckBoxSize, LVI::CheckBoxSize);
+}
+
+void ListViewItem::setupTextEdit()
+{
+    textEdit_->setReadOnly(true);
+    textEdit_->setWordWrapMode(QTextOption::NoWrap);
+    textEdit_->setTextInteractionFlags(Qt::NoTextInteraction);
+    textEdit_->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    textEdit_->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    textEdit_->setFixedWidth(width_ - LVI::CheckBoxSize -
+                             LVI::ButtonSize - 4 * LVI::MarginValue);
+}
+
+void ListViewItem::setupButton()
+{
+    button_->setFixedSize(LVI::ButtonSize, LVI::ButtonSize);
+}
+
+QHBoxLayout* ListViewItem::setupLayout()
+{
+    QHBoxLayout* layout = new QHBoxLayout(this);
+
+    int vertialMargin = (LVI::Height - textEdit_->geometry().height()) / 2;
+
+    layout->setSpacing(LVI::MarginValue);
+    layout->setContentsMargins(LVI::MarginValue, vertialMargin, LVI::MarginValue, vertialMargin);
+
+    layout->addWidget(checkBox_);
+    layout->addWidget(textEdit_);
+    layout->addWidget(button_);
+
+    return layout;
+}
--- listviewitem.h
+++ listviewitem.h
@@ -15,61 +15,17 @@ class ListViewItem : public QWidget
 {
 public:
 
-    explicit ListViewItem(const QString& text, QWidget* parent = nullptr) : QWidget(parent),
-        parent_(parent), width_(parent->size().width()), height_(LVI::Height)
-    {
-        checkBox_ = new QCheckBox(this);
-        textEdit_ = new QTextEdit(text, this);
-        button_ = new QPushButton(this);
-
-        setupCheckBox();
-        setupTextEdit();
-        setupButton();
-
-        this->setLayout(setupLayout());
-        this->setMinimumSize(width_, height_);
-    }
+    explicit ListViewItem(const QString&, QWidget* = nullptr);
 
 private:
 
-    void setupCheckBox()
-    {
-        checkBox_->setCheckState(Qt::Checked);
-        checkBox_->setFixedSize(LVI::CheckBoxSize, LVI::CheckBoxSize);
-    }
+    void setupCheckBox();
 
-    void setupTextEdit()
-    {
-        textEdit_->setReadOnly(true);
-        textEdit_->setWordWrapMode(QTextOption::NoWrap);
-        textEdit_->setTextInteractionFlags(Qt::NoTextInteraction);
-        textEdit_->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-        textEdit_->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    void setupTextEdit();
 
-        textEdit_->setFixedWidth(width_ - LVI::CheckBoxSize -
-                                 LVI::ButtonSize - 4 * LVI::MarginValue);
-    }
+    void setupButton();
 
-    void setupButton()
-    {
-        button_->setFixedSize(LVI::ButtonSize, LVI::ButtonSize);
-    }
-
-    QHBoxLayout* setupLayout()
-    {
-        QHBoxLayout* layout = new QHBoxLayout(this);
-
-        int vertialMargin = (LVI::Height - textEdit_->geometry().height()) / 2;
-
-        layout->setSpacing(LVI::MarginValue);
-        layout->setContentsMargins(LVI::MarginValue, vertialMargin, LVI::MarginValue, vertialMargin);
-
-        layout->addWidget(checkBox_);
-        layout->addWidget(textEdit_);
-        layout->addWidget(button_);
-
-        return layout;
-    }
+    QHBoxLayout* setupLayout();
 
 private:
 
